d85e8da10d1d155b0e2ef97ce76e39296a230865
sara <saralundkvist86@gmail.com>
2020-09-20 13:09:55 +0200

    in spec/support creates a new file response_json.rb with a module that will parse the bode of the server response back to JSON. Creating specs for User registrations, registrations_spec.rb, and for User authentication, sessions_spec.rb. Adding settings in the devise token auth initializer for future work with the client side

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,13 +3,11 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
+require "spec_helper"
 require "rspec/rails"
-
 
 ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -18,5 +16,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
-
+  config.include ResponseJSON
 end
